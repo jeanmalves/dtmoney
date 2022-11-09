a7531cd73ca4efd0ba4c04949213c601a228ef08
Jeam Alves <jeanmalves@gmail.com>
2022-11-09 12:48:37 -0400

    Added paymnet type in transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,12 +8,11 @@ import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import { useTransactions } from '../../hooks/useTransactions';
 import { api } from '../../services/api';
 import Select from 'react-select';
-
-export interface Category {
+export interface Taxonomy {
   id: number;
   name: string;
 }
-interface CategoryOption {
+interface SelectOption {
   value: number;
   label: string;
 }
@@ -24,17 +23,18 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   const { createTransaction } = useTransactions();
-  const [categoryOptions, setCategoryOptions] = useState<CategoryOption[]>([]);
+  const [categoryOptions, setCategoryOptions] = useState<SelectOption[]>([]);
+  const [paymentTypeOptions, setPaymentTypeOptions] = useState<SelectOption[]>([]);
 
   const [title, setTitle] = useState('');
-  const [categoryOptionSelected, setCategoryOptionSelected] = useState<CategoryOption | null>();
+  const [categoryOptionSelected, setCategoryOptionSelected] = useState<SelectOption | null>();
+  const [paymentTypeOptionSelected, setPaymentTypeOptionSelected] = useState<SelectOption | null>();
   const [amount, setAmount] = useState(0);
   const [type, setType] = useState('deposit');
 
-
   useEffect(() => {
     api.get('/categories').then(response => {
-      const categories = response.data.categories.map((category: Category) => {
+      const categories = response.data.categories.map((category: Taxonomy) => {
         return {
           value: category.id,
           label: category.name
@@ -42,6 +42,17 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       });
 
       setCategoryOptions(categories);
+    });
+
+    api.get('/paymentTypes').then(response => {
+      const paymentTypes = response.data.paymentTypes.map((paymentType: Taxonomy) => {
+        return {
+          value: paymentType.id,
+          label: paymentType.name
+        }
+      });
+
+      setPaymentTypeOptions(paymentTypes);
     });
   }, []);
 
@@ -53,23 +64,34 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       name: ''
     };
 
+    const paymentType = {
+      id: 0,
+      name: ''
+    };
+
     if (categoryOptionSelected) {
       category.id = categoryOptionSelected.value
       category.name = categoryOptionSelected.label
+    }
+
+    if (paymentTypeOptionSelected) {
+      paymentType.id = paymentTypeOptionSelected.value
+      paymentType.name = paymentTypeOptionSelected.label
     }
 
     await createTransaction({
       title,
       amount,
       type,
-      category
+      category,
+      paymentType,
     });
 
     setTitle('');
     setAmount(0);
     setType('deposit');
     setCategoryOptionSelected(null);
-
+    setPaymentTypeOptionSelected(null);
 
     onRequestClose();
   }
@@ -149,6 +171,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           noOptionsMessage={() => "Categoria não encontrada."}
           styles={customStyles}
         />
+
+        {
+          type === 'withdraw' &&
+          <Select
+            placeholder="Escolha uma forma de pagamento"
+            defaultValue={paymentTypeOptionSelected}
+            onChange={(selectedPaymentType) => setPaymentTypeOptionSelected(selectedPaymentType)}
+            options={paymentTypeOptions}
+            noOptionsMessage={() => "forma de pagamento não encontrada."}
+            styles={customStyles}
+          />
+        }
 
         <button type="submit">Cadastrar</button>
       </Container>
